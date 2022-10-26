39841ec74c4bce2bc20ce68f1442fa64f5f98885
Zoltan Varadi <contact@data-i-consulting.com>
2022-10-27 07:09:21 +0900

    :+1: typo

--- agent.py
+++ agent.py
@@ -92,7 +92,7 @@ class Agent:
         self.trainer.train_step(state,action,reward,next_state,done)
 
     def get_action(self,state):
-        # random moves: tradeoff explotation / exploitation
+        # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_game
         final_move = [0,0,0]
         if(random.randint(0,200)<self.epsilon):
